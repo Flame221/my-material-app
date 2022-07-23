a647c1c97aa12916fe3b06d26fcd39e8e2d9dba2
Flame221 <43928831+Flame221@users.noreply.github.com>
2022-07-23 20:56:21 +0200

    Added Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Alert,Button, TextField} from '@mui/material';
+import BasicModal from './ModalComponent';
+import { useState } from 'react';
+
+
 
 function App() {
+  let [value, setValue] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Write your name:</h1>
+      <TextField 
+        id="outlined-basic"
+        variant="outlined"
+        onChange={(e) => setValue(e.target.value)}
+        />
+        <BasicModal text={value}/>
     </div>
   );
 }
